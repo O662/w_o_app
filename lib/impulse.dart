e18821d5c9e241b6ccdf5ce1513e1f10f1d2ca19
Malcolm <buildingwithkieth@outlook.com>
2025-01-03 15:36:04 -0600

    Added a bulleted points to impulse page & set necessityMin to 56

--- lib/impulse.dart
+++ lib/impulse.dart
@@ -7,19 +7,19 @@ class ImpulsePage extends StatefulWidget {
 
 class _ImpulsePageState extends State<ImpulsePage> {
   final List<Map<String, dynamic>> _questions = [
-    {'question': 'Have I thought about it for at least two weeks?', 'type': 'yesno', 'points': {'yes': 1, 'no': 0}},
-    {'question': "Does it solve a problem that I've genuinely noticed?", 'type': 'yesno', 'points': {'yes': 1, 'no': 0}},
-    {'question': 'Do I already own something similar?', 'type': 'yesno', 'points': {'yes': 0, 'no': 1}},
-    {'question': 'Is buying it worth giving up progress towards my next financial goal?', 'type': 'yesno', 'points': {'yes': 1, 'no': 0}},
-    {'question': 'Where will it be in five years?', 'type': 'custom', 'options': ['Used or in use', 'Not in use'], 'points': {'Used or in use': 1, 'Not in use': 0}},
+    {'question': 'Have I thought about it for at least two weeks?', 'type': 'yesno', 'points': {'yes': 2, 'no': 0}},
+    {'question': "Does it solve a problem that I've genuinely noticed?", 'type': 'yesno', 'points': {'yes': 2, 'no': 0}},
+    {'question': 'Do I already own something similar?', 'type': 'yesno', 'points': {'yes': 0, 'no': 2}},
+    {'question': 'Is buying it worth giving up progress towards my next financial goal?', 'type': 'yesno', 'points': {'yes': 2, 'no': 0}},
+    {'question': 'Where will it be in five years?', 'type': 'custom', 'options': ['Used', 'In use', 'Not in use'], 'points': {'Used':1, 'In use': 3, 'Not in use': 0}},
     {'question': 'Where will I put it if I buy it?', 'type': 'custom', 'options': ['I have a place in mind', 'Not Sure'], 'points': {'I have a place in mind': 1, 'Not Sure': 0}},
     {'question': 'How long will I have to work in order to pay for it?', 'type': 'custom', 'options': ['Not very long', 'A significant amount of time'], 'points': {'Not very long': 1, 'A significant amount of time': 0}},
-    {'question': 'Can I be productive and happy without it?', 'type': 'yesno', 'points': {'yes': 0, 'no': 1}},
+    {'question': 'Can I be productive and happy without it?', 'type': 'yesno', 'points': {'yes': 0, 'no': 2}},
     {'question': 'What is the cost of it per use?', 'type': 'custom', 'options': ['Worth my money', 'Not worth my money/Unsure'], 'points': {'Worth my money': 1, 'Not worth my money/Unsure': 0}},
-    {'question': 'Does buying it support my priorities?', 'type': 'yesno', 'points': {'yes': 1, 'no': 0}},
+    {'question': 'Does buying it support my priorities?', 'type': 'yesno', 'points': {'yes': 2, 'no': 0}},
     {'question': 'Is this the best way for me to obtain it?', 'type': 'yesno', 'points': {'yes': 1, 'no': 0}},
     {'question': 'Is it a high-quality item with a reasonable price tag?', 'type': 'yesno', 'points': {'yes': 1, 'no': 0}},
-    {'question': 'What is my current mental state?', 'type': 'custom', 'options': ['Calm and neutral', 'Altered by internal or external forces'], 'points': {'Calm and neutral': 1, 'Altered by internal or external forces': 0}},
+    {'question': 'What is my current mental state?', 'type': 'custom', 'options': ['Calm and neutral', 'Altered by internal or external forces'], 'points': {'Calm and neutral': 2, 'Altered by internal or external forces': 0}},
     {'question': "What is the real reason I'm considering buying it?", 'type': 'custom', 'options': ['Need', 'Want', 'Impulse'], 'points': {'Need': 3, 'Want': 1, 'Impulse': 0}},
   ];
 
@@ -30,7 +30,8 @@ class _ImpulsePageState extends State<ImpulsePage> {
     int maxScore = 0;
     int proCount = 0;
     int conCount = 0;
-    String additionalConsiderations = '';
+    int necessityMin= 56;
+    List<String> additionalConsiderations = [];
 
     for (int i = 0; i < _responses.length; i++) {
       final question = _questions[i];
@@ -54,13 +55,13 @@ class _ImpulsePageState extends State<ImpulsePage> {
 
         // Add additional considerations based on specific responses
         if (i == 0 && response == false) {
-          additionalConsiderations += 'Consider thinking about it for at least two weeks before continuing.\n\n';
+          additionalConsiderations.add('Consider thinking about it for at least two weeks before continuing.');
         } else if (i == 1 && response == false) {
-          additionalConsiderations += 'Consider if you actually want this item.\n\n';
+          additionalConsiderations.add("You mentioned this won't solve a problem, so it's recommended you consider if you actually want this item.");
         } else if (i == 2 && response == true) {
-          additionalConsiderations += 'Consider if the item you already own can be repurposed or modified to take the place of this item.\n\n';
+          additionalConsiderations.add('Consider if the item you already own can be repurposed or modified to take the place of this item.');
         } else if (i == 5 && response == 'Not Sure') {
-          additionalConsiderations += 'Consider if you have the room for this item and if you can reasonably find a permanent home for it.\n\n';
+          additionalConsiderations.add('Consider if you have the room for this item and if you can reasonably find a permanent home for it.');
         }
       }
 
@@ -75,34 +76,34 @@ class _ImpulsePageState extends State<ImpulsePage> {
     }
 
     double percentageScore = (totalScore / maxScore) * 100;
-    String recommendation = percentageScore >= 56
+    String recommendation = percentageScore >= necessityMin
         ? 'You can go ahead and purchase the item!'
         : 'It is recommended not to purchase the item.';
 
     // Additional considerations based on specific responses
-    if (percentageScore >= 56 && _responses[6] == 'A significant amount of time') {
-      additionalConsiderations += 'You mentioned this will take a long time to pay for it, are you positive this item would be worth it?\n\n';
+    if (percentageScore >= necessityMin && _responses[6] == 'A significant amount of time') {
+      additionalConsiderations.add('You mentioned this will take a long time to pay for it, are you positive this item would be worth it?');
     }
     if (_responses[7] == true) {
-      additionalConsiderations += 'If you can be productive without it, are you sure you need it?\n\n';
+      additionalConsiderations.add('If you can be productive without it, are you sure you need it?');
     }
     if (_responses[8] == 'Not worth my money/Unsure') {
-      additionalConsiderations += 'You said this is not worth your money, are you positive you want it?\n\n';
+      additionalConsiderations.add('You said this is not worth your money, are you positive you want it?');
     }
     if (_responses[9] == false) {
-      additionalConsiderations += 'You said this does not support your priorities, do you think buying this item is truly worth it?\n\n';
+      additionalConsiderations.add('You said this does not support your priorities, do you think buying this item is truly worth it?');
     }
     if (_responses[10] == false) {
-      additionalConsiderations += 'You said this is not the best way to obtain the item, is there a better way that is cheaper or more likely not to provide a defective item?\n\n';
+      additionalConsiderations.add('You said this is not the best way to obtain the item, is there a better way that is cheaper or more likely not to provide a defective item?');
     }
     if (_responses[11] == false) {
-      additionalConsiderations += 'You said this is not a high-quality item, is this item worth the price, or is there something better at a more reasonable price tag?\n\n';
+      additionalConsiderations.add('You said this is not a high-quality item, is this item worth the price, or is there something better at a more reasonable price tag?');
     }
     if (_responses[12] == 'Altered by internal or external forces') {
-      additionalConsiderations += 'Consider purchasing this item when your mental state is more calm and collected.\n\n';
+      additionalConsiderations.add('Consider purchasing this item when your mental state is more calm and collected.');
     }
     if (_responses[13] == 'Impulse') {
-      additionalConsiderations += 'You said the real reason you are considering buying the item is because of impulse. Evaluate if you actually want the item. If the answer is no, then you should not consider it.\n\n';
+      additionalConsiderations.add('You said the real reason you are considering buying the item is because of impulse. Evaluate if you actually want the item. If the answer is no, then you should not consider it.');
     }
 
     showDialog(
@@ -121,7 +122,7 @@ class _ImpulsePageState extends State<ImpulsePage> {
                 Text('Cons: $conCount'),
                 SizedBox(height: 10),
                 Text('Considerations:'),
-                Text(additionalConsiderations),
+                ...additionalConsiderations.map((consideration) => Text('• $consideration')).toList(),
               ],
             ),
           ),
